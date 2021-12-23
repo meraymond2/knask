91f6561fa9c020a3170dbf746c8b035cec8a6604
Michael Raymond <michael_raymond@fastmail.com>
2021-12-23 09:00:39 +0000

    format

--- main.cpp
+++ main.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
-#include <vector>
 #include <variant>
+#include <vector>
+
 #include "parser.h"
 
 std::string concat_args(char *args[], int size) {
-    std::string concatenated{};
-    if (size > 0) {
-        concatenated.append(args[0]);
-    }
-    for (int i = 1; i < size; i++) {
-        concatenated.append(" ");
-        concatenated.append(args[i]);
-    }
-    return concatenated;
+  std::string concatenated {};
+  // can’t use the range-based for-loop on arrays, so old style loop
+  for (uint64_t i = 0; i < size; i++) {
+    // will add whitespace to the first element, which is ok here,
+    // because whitespace is ignored, except as a delimiter between numbers
+    concatenated.append(" ");
+    concatenated.append(args[i]);
+  }
+  return concatenated;
 }
 
 int main(int argc, char *argv[]) {
-    // 1. Concatenate arguments into string
-    std::string arg_string{concat_args(argv + 1, argc - 1)}; // the + 1 is implicitly + 1 * sizeof(char *)
-    // 2. Tokenise argument-string
-    parser::ParseArgs to_parse {parser::tokenise_args(arg_string)};
-    // 3. Parse bytes according to specs
-    std::vector<parser::Parsed> parsed{parser::parse(to_parse)};
-    // 4. Print output
-    for (parser::Parsed val: parsed) {
-        std::visit(parser::PrintParsed(), val);
-    }
-    return 0;
+  // 1. Concatenate arguments into single string
+  // the pointer + 1 is implicitly ptr + 1 * sizeof(char*)
+  // so it’s not necessary to add the exact # of bytes to the ptr
+  std::string arg_string {concat_args(argv + 1, argc - 1)};
+  // 2. Tokenise argument-string
+  parser::ParseArgs to_parse {parser::tokenise_args(arg_string)};
+  // 3. Parse bytes according to specs
+  std::vector<parser::Parsed> parsed {parser::parse(to_parse)};
+  // 4. Print output
+  for (parser::Parsed val : parsed) {
+    // still a little hazy on std::visit, but as I understand it, it
+    // is the nearest equivalent to pattern matching on a std::variant
+    std::visit(parser::PrintParsed(), val);
+  }
+  return 0;
 }
--- parser.h
+++ parser.h
@@ -1,53 +1,59 @@
+#include <iostream>
 #include <variant>
+#include <vector>
 
 #ifndef KNASK_PARSER_H
 #define KNASK_PARSER_H
+
 namespace parser {
 
-    enum class Spec {
-        u8, u16, u32, u64, i8, i16, i32, i64, f32, f64, str
-    };
+enum class Spec { u8, u16, u32, u64, i8, i16, i32, i64, f32, f64, str };
 
-    typedef std::variant<
-            uint8_t, uint16_t, uint32_t, uint64_t,
-            int8_t, int16_t, int32_t, int64_t,
-            float, double, std::string
-    > Parsed;
+typedef std::variant<uint8_t, uint16_t, uint32_t, uint64_t, int8_t, int16_t,
+                     int32_t, int64_t, float, double, std::string>
+    Parsed;
 
-    struct ParseArgs {
-        std::vector<Spec> specs;
-        std::vector<std::byte> bytes;
+struct ParseArgs {
+  std::vector<Spec> specs;
+  std::vector<std::byte> bytes;
 
-        ParseArgs(std::vector<Spec> specs, std::vector<std::byte> bytes) : specs(std::move(specs)),
-                                                                           bytes(std::move(bytes)) {}
-    };
+  ParseArgs(std::vector<Spec> specs, std::vector<std::byte> bytes)
+      : specs(std::move(specs)), bytes(std::move(bytes)) {}
+};
 
-    ParseArgs tokenise_args(std::string arg_str);
+ParseArgs tokenise_args(std::string arg_str);
 
-    std::vector<Parsed> parse(ParseArgs args);
+std::vector<Parsed> parse(ParseArgs args);
 
-    struct PrintParsed {
-        void operator()(uint8_t &val) { std::cout << "u8: " << static_cast<uint16_t>(val) << std::endl; }
+struct PrintParsed {
+  void operator()(uint8_t &val) {
+    std::cout << "u8: " << static_cast<uint16_t>(val) << std::endl;
+  }
 
-        void operator()(uint16_t &val) { std::cout << "u16: " << val << std::endl; }
+  void operator()(uint16_t &val) { std::cout << "u16: " << val << std::endl; }
 
-        void operator()(uint32_t &val) { std::cout << "u32: " << val << std::endl; }
+  void operator()(uint32_t &val) { std::cout << "u32: " << val << std::endl; }
 
-        void operator()(uint64_t &val) { std::cout << "u64: " << val << std::endl; }
+  void operator()(uint64_t &val) { std::cout << "u64: " << val << std::endl; }
 
-        void operator()(int8_t &val) { std::cout << "i8: " << static_cast<int16_t>(val) << std::endl; }
+  void operator()(int8_t &val) {
+    std::cout << "i8: " << static_cast<int16_t>(val) << std::endl;
+  }
 
-        void operator()(int16_t &val) { std::cout << "i16: " << val << std::endl; }
+  void operator()(int16_t &val) { std::cout << "i16: " << val << std::endl; }
 
-        void operator()(int32_t &val) { std::cout << "i32: " << val << std::endl; }
+  void operator()(int32_t &val) { std::cout << "i32: " << val << std::endl; }
 
-        void operator()(int64_t &val) { std::cout << "i64: " << val << std::endl; }
+  void operator()(int64_t &val) { std::cout << "i64: " << val << std::endl; }
 
-        void operator()(float &val) { std::cout << "f32: " << val << std::endl; }
+  void operator()(float &val) { std::cout << "f32: " << val << std::endl; }
 
-        void operator()(double &val) { std::cout << "f64: " << val << std::endl; }
+  void operator()(double &val) { std::cout << "f64: " << val << std::endl; }
 
-        void operator()(std::string &val) { std::cout << "str: " << val << std::endl; }
-    };
-}
-#endif //KNASK_PARSER_H
+  void operator()(std::string &val) {
+    std::cout << "str: " << val << std::endl;
+  }
+};
+
+} // namespace parser
+#endif // KNASK_PARSER_H
